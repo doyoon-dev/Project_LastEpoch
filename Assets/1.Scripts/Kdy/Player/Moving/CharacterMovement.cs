1fb0c02f3e422295ce143f6b7b891aa7fa3ad113
dy-dev <89628091+doyoon-dev@users.noreply.github.com>
2024-03-31 20:27:42 +0900

    플레이어 이동

---
+++ Assets/1.Scripts/Kdy/Player/Moving/CharacterMovement.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public float m_moveSpeed = 3.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Move(Vector3 target)
+    {
+        StopAllCoroutines();
+        StartCoroutine(Moving(target));
+    }
+
+    IEnumerator Moving(Vector3 target)
+    {
+        Vector3 dir = target - transform.position;
+        float dist = dir.magnitude;
+        dir.Normalize();
+
+        while (dist > 0.0f)
+        {
+            float delta = Time.deltaTime * m_moveSpeed;
+            if(delta > dist) delta = dist;
+            dist -= delta;
+            transform.Translate(dir * delta, Space.World);
+            yield return null;
+        }
+    }
+}
