0c2f4c1506bfe646343126991e8c91da9a785c10
dy-dev <89628091+doyoon-dev@users.noreply.github.com>
2024-05-10 17:13:29 +0900

    슬롯에 들어갈 아이템 사이즈

--- Assets/1.Scripts/Kdy/Inventory/Item.cs
+++ Assets/1.Scripts/Kdy/Inventory/Item.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,6 +9,11 @@ public class Item : MonoBehaviour
     public int m_itemSizeWidth = 1;
     public int m_itemSizeHeight = 1;
 
+    Slot m_slotSize;
+
+    // string에 아이템 이름 -> 나중에 ItemData 만들면 그걸로 바꿔야함
+    Dictionary<string, int[]> m_itemSlotSize = new Dictionary<string, int[]>();
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +23,26 @@ public class Item : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            ItemSize("Knife", 2, 4);
+            Debug.Log(m_itemSlotSize.Values);
+        }
+    }
+
+    void ItemSize(string name, int itemSizeX, int itemSizeY)
+    {
+        int[,] slotSize = new int[itemSizeX, itemSizeY];
+        for (int i = 0; i < itemSizeX; i++)
+        {
+            for(int j = 0; j < itemSizeY; j++)
+            {
+                int slotSizeX = (int)transform.localPosition.x + i;
+                int slotSizeY = (int)transform.localPosition.y + j;
+                //slotSize = new int[] { slotSizeX, slotSizeY };
+                
+            }
+        }
+        //m_itemSlotSize.Add(name, slotSize[,]);
     }
 }
