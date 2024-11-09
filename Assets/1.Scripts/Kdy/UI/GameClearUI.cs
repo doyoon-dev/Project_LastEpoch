8a9e4e656e13070a6fc891be6c3b52fc316df82b
dy-dev <89628091+doyoon-dev@users.noreply.github.com>
2024-11-09 20:51:16 +0900

    241109_dy2

---
+++ Assets/1.Scripts/Kdy/UI/GameClearUI.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameClearUI : MonoBehaviour
+{
+    [SerializeField]
+    GameObject m_playTimeObj;
+    [SerializeField]
+    GameObject m_monCntObj;
+    string m_playTime;
+    int m_monCnt = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
